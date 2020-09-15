cd93291cfcd5a458708389bc1831de59e5a0ebe7
email <nakayamay@woodtec.co.jp>
2020-09-15 10:32:15 +0900

    コンポーザ利用

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,12 +9,8 @@ class HelloController extends Controller
 {
     public function index()
     {
-        $data = [
-            ['name' => '山田たろう', 'mail' => 'taro@yamada'],
-            ['name' => '田中はなこ', 'mail' => 'hanako@flower'],
-            ['name' => '鈴木さちこ', 'mail' => 'sachiko@yamada'],
-        ];
-        return view('hello.index', ['data' => $data]);
+
+        return view('hello.index', ['message' => 'hello']);
     }
 
     public function post(Request $request)
