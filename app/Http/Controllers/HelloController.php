a9b55ddcca2c14a8570b59eb8d3fcf71a9bb80e2
email <nakayamay@woodtec.co.jp>
2020-10-01 13:18:32 +0900

    P161 Cookieの取得・表示

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-// use Illuminate\Http\Response;
+use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Illuminate\Support\Facades\Validator;
 
