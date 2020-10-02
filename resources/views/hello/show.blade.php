64862821164d2981eae3acaeb0f00d326fa99024
email <nakayamay@woodtec.co.jp>
2020-10-02 10:58:23 +0900

    orwhereを使ったあいまい検索

--- resources/views/hello/show.blade.php
+++ resources/views/hello/show.blade.php
@@ -10,24 +10,20 @@
 
 
 @section('content')
-<table>
+@if ($items!=null)
+@foreach ($items as $item)
+
+<table width="400px">
     <tr>
-        <th>id:</th>
-        <td>{{$item->id}}</td>
-    </tr>
-    <tr>
-        <th>name:</th>
+        <th width="50px">id:</th>
+        <td width="50px">{{$item->id}}</td>
+        <th width="50px">name:</th>
         <td>{{$item->name}}</td>
     </tr>
-    <tr>
-        <th>mail:</th>
-        <td>{{$item->mail}}</td>
-    </tr>
-    <tr>
-        <th>age</th>
-        <td>{{$item->age}}</td>
-    </tr>
+
 </table>
+@endforeach
+@endif
 @endsection
 @section('footer')
 Copyright 2017 tuyano.
